80f9f515c00b6e251da5dc1014f2b4b64feff6e4
kwaku96 <kwakukusi30@outlook.com>
2020-08-17 15:11:17 +0300

    first

---
+++ bleak_send.py
@@ -0,0 +1,67 @@
+import platform
+import asyncio
+import logging
+
+from bleak import BleakClient
+
+data_send = bytearray([0xA0,0xF5,0x44])
+
+async def run(address, loop, debug=False):
+    log = logging.getLogger(__name__)
+    if debug:
+        import sys
+
+        loop.set_debug(True)
+        log.setLevel(logging.DEBUG)
+        h = logging.StreamHandler(sys.stdout)
+        h.setLevel(logging.DEBUG)
+        log.addHandler(h)
+
+    async with BleakClient(address, loop=loop) as client:
+        x = await client.is_connected()
+        log.info("Connected: {0}".format(x))
+
+        for service in client.services:
+            log.info("[Service] {0}: {1}".format(service.uuid, service.description))
+            for char in service.characteristics:
+                if "write" in char.properties:
+                    try:
+                        await client.write_gatt_char(char.uuid,data_send)
+                        await asyncio.sleep(1)
+                        print("data has been sent")
+                    except Exception as e:
+                        print(e)
+                continue
+                if "read" in char.properties:
+                    try:
+                        value = bytes(await client.read_gatt_char(char.uuid))
+                    except Exception as e:
+                        value = str(e).encode()
+                else:
+                    value = None
+                log.info(
+                    "\t[Characteristic] {0}: (Handle: {1}) ({2}) | Name: {3}, Value: {4} ".format(
+                        char.uuid,
+                        char.handle,
+                        ",".join(char.properties),
+                        char.description,
+                        value,
+                    )
+                )
+                for descriptor in char.descriptors:
+                    value = await client.read_gatt_descriptor(descriptor.handle)
+                    log.info(
+                        "\t\t[Descriptor] {0}: (Handle: {1}) | Value: {2} ".format(
+                            descriptor.uuid, descriptor.handle, bytes(value)
+                        )
+                    )
+
+
+if __name__ == "__main__":
+    address = (
+        "24:71:89:cc:09:05"
+        if platform.system() != "Darwin"
+        else "B9EA5233-37EF-4DD6-87A8-2A875E821C46"
+    )
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(address, loop, True))
